decd2601245f183dddb98fdc2c73e14fb11b1ad2
Yunior <maucaylle72005@gmail.com>
2025-12-12 18:47:21 -0500

    mas ajustes

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,16 +1,22 @@
-import { Router,CanActivateFn } from '@angular/router';
+import { Router, CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../../services/auth';
+import { map, take } from 'rxjs/operators';
 
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.isAuthenticated()) {
-    return true;
-  }
-
-  router.navigate(['/login']);
-  return false;
+  // Esperar a que Firebase Auth verifique la sesión
+  return authService.user$.pipe(
+    take(1), // Solo tomar el primer valor
+    map(user => {
+      if (user) {
+        return true;
+      }
+      router.navigate(['/login']);
+      return false;
+    })
+  );
 };
 
